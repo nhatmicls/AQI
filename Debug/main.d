bebfd1380afcf90b93c5549436afda36b80366f3
Micls <cptprice123@outlook.com>
2019-01-18 11:17:52 +0700

    Update 3
    Time to LCD

--- Debug/main.d
+++ Debug/main.d
@@ -13,6 +13,10 @@ main.obj: ../lib_RNG.h
 main.obj: ../lib_lcd.h
 main.obj: ../delay.h
 main.obj: ../const_data.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/stdlib.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/_ti_config.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/linkage.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/cdefs.h
 
 ../main.c: 
 C:/ti/ccsv8/ccs_base/msp430/include/msp430.h: 
@@ -27,3 +31,7 @@ C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics_legacy_un
 ../lib_lcd.h: 
 ../delay.h: 
 ../const_data.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/stdlib.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/_ti_config.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/linkage.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/cdefs.h: 
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "lib_lcd.h"
 #include "delay.h"
 #include "const_data.h"
-
+#include <stdlib.h>
 
 #define C_BORDER    0x0005
 #define s_num       110
@@ -14,6 +14,8 @@
 void clean_area(uint);
 void init_star(uchar);
 void get_star_pos(uchar);
+
+char index1[5];
 
 uchar space_char=1;     // space between chars
 struct point3d_schar dots3d[s_num];
@@ -27,6 +29,31 @@ sint x=0,y=0,x1=0,y1=0,x2=0,y2=0,r=0,i=0,z=0;
 
 int counter=0,timehms[3]={0,0,0};
 uchar cache[5];
+
+void itoa(long unsigned int value, char* result, int base)
+{
+      // check that the base if valid
+    if (base < 2 || base > 36) { *result = '\0';}
+    char* ptr = result, *ptr1 = result, tmp_char;
+    int tmp_value;
+
+    do
+    {
+        tmp_value = value;
+        value /= base;
+        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+    } while ( value );
+    // Apply negative sign
+    if (tmp_value < 0)
+        *ptr++ = '-';
+    *ptr-- = '\0';
+    while(ptr1 < ptr)
+    {
+        tmp_char = *ptr;
+        *ptr--= *ptr1;
+        *ptr1++ = tmp_char;
+    }
+}
 
 void clock_init()
 {
@@ -73,10 +100,24 @@ int main(void)
     init();
     init_USCI();                // init. USCI (SPI)
     init_LCD(C_BLACK);          // init. Display
-    while (1)
+    refe(0);
+    while(1)
     {
-        draw_string(10, 20, "ff", C_RED, TRANSP, 0, 0);
+        _no_operation();
+        if(timehms[2]==10)
+            refe(1);
     }
+}
+
+void refe(int n)
+{
+    if(n==1)
+        clean_area((C_BLACK));
+    draw_string(2, 2, string_00, C_RED, TRANSP, 1, 1);
+    wait_us(1);
+    draw_string(2, 15, string_01, C_RED, TRANSP, 1, 1);
+    itoa(timehms[2],index1,10);
+    draw_string(35, 2, index1, C_RED, TRANSP, 1, 1);
 }
 
 #pragma vector=TIMER0_A0_VECTOR
